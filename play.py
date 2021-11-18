a621815e1ba442253e5a41db717a0f75734b4090
RickyBalboa33 <balboa33@zohomail.com>
2021-11-18 17:45:54 +0000

    Initial Commit

--- play.py
+++ play.py
@@ -1,29 +1,46 @@
-n = 200
-wzr =8.65
-dcx= 8.75
-bttinr= 0.2525
-btttrx=0.03232
+
+wzr_trx =8.40
+dcx_trx= 8.50
+W_bttinr= 0.2640
+D_bttinr = 0.27223
+btttrx=0.03225
 trx=0
 btt=0
-cap=1730
+cap=100
 p=0
-def m(n,cap,p,wzr,dcx,btttrx,bttinr,trx,btt):
-    if(cap>100):
-        n-=1
-        ad = n*dcx
-        print("TRX # :",n," |TRXINR:",dcx," |TRX*INR:",ad)
-        p += ad-cap
-        print("Profit: ",round(p))
-        n-=1
-        print("TRX # :",n)
-        btt = round(n/btttrx)
-        print("TRX # :",n," |TRXBTT:",btttrx," |TRX/BTT:",btt)
-        sb = btt*bttinr
-        print("BTT:",btt," |BTTINR: ",bttinr," |BTT*INR:",sb)
-        p =cap-sb
-        cap = sb
-        print("Cap: ",cap)
-        print("P: ",p)
-    # m(n,cap,p,wzr,dcx,btttrx,bttinr,trx,btt)
+orders=4
+cur_order=0
+flag=True
+def m(cap,p,wzr_trx,dcx_trx,btttrx,W_bttinr,D_bttinr,trx,btt,flag,orders,cur_order):
+    
+    if(btt<=0 and flag):
+        btt = round(cap/D_bttinr)
+        print("BIN # BTT: ",btt)
+        trx=round(btt*btttrx)
+        print("Bin, Trx #: ",trx)
+        # trx-=1
+        flag=False
+    if(btt<=0 and not flag):
+        btt = round(cap/D_bttinr)
+        print("DCX # BTT: ",btt)
+        trx=round(btt*btttrx)
 
-m(n,cap,p,wzr,dcx,btttrx,bttinr,trx,btt)
+    if(btt>0):
+        print("On DCX, Trx #: ",trx)
+        temp_cap= round(trx*dcx_trx)
+        print("Cap After TRX sell: ",temp_cap)
+        btt=0
+        trx=0
+        p += (temp_cap-cap)
+        print("Profit: ",p," |Cap: ",cap)
+    if(orders>cur_order):
+        cur_order+=1
+        print("++++++++++++++++ Order Number: ",cur_order," +++++++++++++")
+        m(cap,p,wzr_trx,dcx_trx,btttrx,W_bttinr,D_bttinr,trx,btt,flag,orders,cur_order)
+
+
+
+        
+    
+
+m(cap,p,wzr_trx,dcx_trx,btttrx,W_bttinr,D_bttinr,trx,btt,flag,orders,cur_order)
